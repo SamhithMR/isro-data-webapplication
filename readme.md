85596ea5dff63658284b7edabe332395517c5fbb
samhithMR <samhith.mr@gmail.com>
2023-04-07 19:29:33 +0530

    implemented seach funcitonaliry

---
+++ readme.md
@@ -0,0 +1,36 @@
+# ISRO Satellites Website
+
+This website displays information about Indian Space Research Organisation (ISRO) satellites using the API provided by https://isro.vercel.app/. The website was built using HTML, CSS, and JavaScript.
+
+## Live Demo
+
+You can view a live demo of the website at https://isro-data-webapplication.netlify.app/.
+
+## Screenshots
+
+Here are some screenshots of the website:
+
+![Screenshot 1](./assets/preview.png)
+![Screenshot 2](./assets/preview2.png)
+
+## Installation
+
+To run the website locally, you can follow these steps:
+
+1. Clone the repository: `git clone https://github.com/samhithMR/isro-data-webapplication.git`
+2. Open the `index.html` file in your web browser.
+
+## Features
+
+- Spacecraft Information: Browse detailed information on various spacecraft launched by ISRO.
+- Launcher Information: Explore a list of all the launch vehicles used by ISRO for its missions.
+- Customer Satellites Information: Access detailed information on the customer satellites launched by ISRO.
+- Search by Country: Use the search functionality to filter the list of customer satellites based on the country of origin.
+- Search by Center: Filter the list of ISRO missions by launch center using the search functionality.
+- completely responsive
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+// select all elements with class "itemss"
 const itemss = document.querySelectorAll('.itemss');
+
+// select all elements with class "item"
 const items = document.querySelectorAll('.item');
 
+// add event listener to each element with class "itemss"
 itemss.forEach((x, i) => {
   x.addEventListener('click', () => {
+
+    // remove "clicked" class from all elements with class "itemss"
+    itemss.forEach((item)=>{
+        item.classList.remove('clicked')
+    })
+
+    // add "clicked" class to the clicked element
+    x.classList.add('clicked');
+
+    // show the corresponding element with class "item" and hide others
     items.forEach((y, j) => {
       if (i === j) {
         y.classList.add('display');
@@ -16,7 +30,10 @@ itemss.forEach((x, i) => {
 });
 
 
+// select the element with class "spacecrafts"
 const spacecrafts = document.querySelector(".spacecrafts");
+
+// fetch data from the API and display it
 fetch('https://isro.vercel.app/api/spacecrafts')
   .then(response => response.json())
   .then(data => {
@@ -33,7 +50,10 @@ fetch('https://isro.vercel.app/api/spacecrafts')
   });
 
 
+// select the element with class "launchers"
 const launchers = document.querySelector(".launchers");
+
+// fetch data from the API and display it
 fetch('https://isro.vercel.app/api/launchers')
   .then(response => response.json())
   .then(data => {
@@ -49,67 +69,68 @@ fetch('https://isro.vercel.app/api/launchers')
     console.error(error);
   });
 
-const csItems = document.querySelector(".csItems");
-var info = [];
-fetch('https://isro.vercel.app/api/customer_satellites')
-  .then(response => response.json())
-  .then(data => {
+
+  const csItems = document.querySelector(".csItems");
+  var info = [];
+  fetch('https://isro.vercel.app/api/customer_satellites')
+    .then(response => response.json())
+    .then(data => {
       data?.customer_satellites?.forEach((item, index) => {
-        info.push(item)
-        const div = document.createElement("div");
-        div.textContent = `${item.id} ${item.country} ${item.launch_date} ${item.mass} ${item.launcher}`;
-        div.classList.add(index % 2 === 0 ? "even" : "odd");
-        csItems.appendChild(div);
+        info.push(item);
+        const tr = document.createElement("tr");
+        tr.innerHTML = `<td>${item.id}</td><td>${item.country}</td><td>${item.launch_date}</td><td>${item.mass}</td><td>${item.launcher}</td>`;
+        tr.classList.add(index % 2 === 0 ? "even" : "odd");
+        csItems.appendChild(tr);
+      });
+      console.log(info);
+    })
+    .catch(error => {
+      console.error(error);
     });
-    console.log(info);
-  })
-  .catch(error => {
-    console.error(error);
+  
+  const search = document.querySelector('.search');
+  search.addEventListener('input', ()=>{
+      const filteredInfo = info?.filter((item)=>{ 
+          return item.country.toLowerCase() == search.value.toLowerCase();
+      });
+      csItems.innerHTML = '';
+      filteredInfo.forEach((item, index) => {
+          const tr = document.createElement("tr");
+          tr.innerHTML = `<td>${item.id}</td><td>${item.country}</td><td>${item.launch_date}</td><td>${item.mass}</td><td>${item.launcher}</td>`;
+          tr.classList.add(index % 2 === 0 ? "even" : "odd");
+          csItems.appendChild(tr);
+      });
   });
-
-const search = document.querySelector('.search');
-search.addEventListener('input', ()=>{
-    const filteredInfo = info?.filter((item)=>{ 
-        return item.country.toLowerCase() == search.value.toLowerCase();
-    });
-    csItems.innerHTML = '';
-    filteredInfo.forEach((item, index) => {
-        const div = document.createElement("div");
-        div.textContent = `${item.id} ${item.country} ${item.launch_date} ${item.mass} ${item.launcher}`;
-        div.classList.add(index % 2 === 0 ? "even" : "odd");
-        csItems.appendChild(div);
-    });
-});
+  
 
 
-const center = document.querySelector(".center");
-var info2 = [];
-fetch('https://isro.vercel.app/api/centres')
-  .then(response => response.json())
-  .then(data => {
+  const centers = document.querySelector(".centers");
+  var info2 = [];
+  fetch('https://isro.vercel.app/api/centres')
+    .then(response => response.json())
+    .then(data => {
       data?.centres?.forEach((item, index) => {
-        info2.push(item)
-        const div = document.createElement("div");
-        div.textContent = `${item.id} ${item.name} ${item.Place} ${item.State} `;
-        div.classList.add(index % 2 === 0 ? "even" : "odd");
-        center.appendChild(div);
+        info2.push(item);
+        const tr = document.createElement("tr");
+        tr.innerHTML = `<td>${item.id}</td><td>${item.name}</td><td>${item.Place}</td><td>${item.State}</td>`;
+        tr.classList.add(index % 2 === 0 ? "even" : "odd");
+        centers.appendChild(tr);
+      });
+    })
+    .catch(error => {
+      console.error(error);
     });
-    console.log(info2);
-  })
-  .catch(error => {
-    console.error(error);
+  
+  const search2 = document.querySelector('.search2');
+  search2.addEventListener('input', ()=>{
+      const filteredInfo2 = info2?.filter((item)=>{ 
+          return item.State.toLowerCase() == search2.value.toLowerCase();
+      });
+      centers.innerHTML = '';
+      filteredInfo2.forEach((item, index) => {
+          const tr = document.createElement("tr");
+          tr.innerHTML = `<td>${item.id}</td><td>${item.name}</td><td>${item.Place}</td><td>${item.State}</td>`;
+          tr.classList.add(index % 2 === 0 ? "even" : "odd");
+          centers.appendChild(tr);
+      });
   });
-
-const search2 = document.querySelector('.search2');
-search2.addEventListener('input', ()=>{
-    const filteredInfo2 = info2?.filter((item)=>{ 
-        return item.State.toLowerCase() == search2.value.toLowerCase();
-    });
-    center.innerHTML = '';
-    filteredInfo2.forEach((item, index) => {
-        const div = document.createElement("div");
-        div.textContent = `${item.id} ${item.name} ${item.Place} ${item.State}`;
-        div.classList.add(index % 2 === 0 ? "even" : "odd");
-        center.appendChild(div);
-    });
-});
