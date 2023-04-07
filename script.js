7542665929431b08643afca761eb6fc6d5a4170c
samhithMR <samhith.mr@gmail.com>
2023-04-07 17:00:37 +0530

    built a function for fetching launchers endpoint

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const spacecrafts = document.querySelector(".spacecrafts");
+const itemss = document.querySelectorAll('.itemss');
+const items = document.querySelectorAll('.item');
 
+itemss.forEach((x, i) => {
+  x.addEventListener('click', () => {
+    items.forEach((y, j) => {
+      if (i === j) {
+        y.classList.add('display');
+        y.classList.remove('hidden');
+      } else {
+        y.classList.remove('display');
+        y.classList.add('hidden');
+      }
+    });
+  });
+});
+
+
+const spacecrafts = document.querySelector(".spacecrafts");
 fetch('https://isro.vercel.app/api/spacecrafts')
   .then(response => response.json())
   .then(data => {
@@ -15,21 +32,21 @@ fetch('https://isro.vercel.app/api/spacecrafts')
     console.error(error);
   });
 
-  
-  const itemss = document.querySelectorAll('.itemss');
-  const items = document.querySelectorAll('.item');
-  
-  itemss.forEach((x, i) => {
-    x.addEventListener('click', () => {
-  
-      items.forEach((y, j) => {
-        if (i === j) {
-          y.classList.add('display');
-          y.classList.remove('hidden');
-        } else {
-          y.classList.remove('display');
-          y.classList.add('hidden');
-        }
-      });
+
+const launchers = document.querySelector(".launchers");
+fetch('https://isro.vercel.app/api/launchers')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    data?.launchers?.forEach((item, index) => {
+        const div = document.createElement("div");
+        div.textContent = `${item.id}`;
+        div.classList.add(index % 2 === 0 ? "even" : "odd");
+        launchers.appendChild(div);
     });
+  })
+  .catch(error => {
+    console.error(error);
   });
+
+  
